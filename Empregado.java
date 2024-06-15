867d317828816f83eb2c1d3350a98e01130e695d
AlexPMendonca <168931950+AlexPMendonca@users.noreply.github.com>
2024-06-14 22:02:25 -0300

    Add files via upload

---
+++ Empregado.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.unid5code1;
+
+/**
+ *
+ * @author Alex
+ */
+class Empregado {
+    String nome;
+    String sobrenome;
+    int matricula;
+    float salário;
+}
---
+++ Unid5Code1.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.unid5code1;
+
+/**
+ *
+ * @author Alex
+ */
+public class Unid5Code1 {
+
+    public static void main(String[] args) {
+        
+        Empregado func = new Empregado ();
+        
+        func.nome = "Alex";
+        func.sobrenome = "Mendonca";
+        func.matricula = 358;
+        func.salário = 2234.54f;
+        
+        System.out.println("Nome do Funcionario: " + func.nome + " " + func.sobrenome);
+        System.out.println("Numero da Matricula: " + func.matricula);
+        System.out.println("Salário: R$ " + func.salário);
+    }}
+    
+        
+    
+
